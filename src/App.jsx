891209e55d8a2b0c7ce8bc77c9dc8d8954514c38
khushimehra <akmkhushimehra02@gmail.com>
2025-07-20 16:12:39 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 // import './App.css'
-import WeatherApp from "./WeatherApp"
+import WeatherApp from "./WeatherApp";
 
 function App() {
-   return (
+  return (
     <>
-      <WeatherApp/>
+      <WeatherApp />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
